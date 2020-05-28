e6d7df789635d9f0fc9f8feb9af0bd5076aa4c83
a402942988 <402942988@qq.com>
2020-05-28 14:25:09 +0800

    123

--- demo/src/main/java/com/example/demo/controller/Controller.java
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -3,12 +3,15 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class Controller {
 
+    Date date = new Date();
     @RequestMapping("hello")
     public String hello(String name){
-        return name + "，<font color='red'>您好</font>";
+        return name + "，<font color='red'>您好</font>" + date.getTime();
     }
 
 }
